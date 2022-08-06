9187b3c7114a87eaa6c89efef166df9cbf27bbaa
Luojiax <72960890+Luojiax@users.noreply.github.com>
2022-08-07 01:44:11 +0800

    Update step1_pre_for_HoloNet_input_data.R

--- step4_tools_intra_benchmark/HoloNet/step1_pre_for_HoloNet_input_data.R
+++ step4_tools_intra_benchmark/HoloNet/step1_pre_for_HoloNet_input_data.R
@@ -10,14 +10,9 @@ load('./step1_data_process/se_data/ST.se.RData')
 raw.count <- GetAssayData(se.st, "count", "Spatial")
 raw.count <- as.matrix(raw.count)
 write.csv(raw.count, "./step4_tools_intra_benchmark/HoloNet/input/CID4465_raw_count.csv", quote=F)
-se.st <- SCTransform(se.st, "Spatial")
-norm.data <- GetAssayData(se.st, "data", "SCT")
-norm.data <- as.matrix(norm.data)
-write.csv(norm.data, "./step4_tools_intra_benchmark/HoloNet/input/CID4465_norm_count.csv", quote=F)
 
 # load cell type metadata
 if(T){
-  load("./step3_benchmark/ST_benchmark_data/ST.CID4465.meta.RData")
   meta <- se.st@meta.data
   meta$celltype <- stringr::str_replace_all(meta$celltype,"B_cells", "B-cells")
   meta$celltype <- stringr::str_replace_all(meta$celltype, "T_cells", "T-cells")
